5b5a0adddbbff10227c85a94f76cb7c69f7d19cd
Jamon Dixon <jamonadixon@gmail.com>
2021-01-15 21:59:37 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -4,7 +4,7 @@ def using_concat(first_array, second_array)
   first_array.concat(second_array)
 end
 
-def using_insert(array, element)
+def using_insert(array,element)
   new_array = array.insert(element)
 end
 
